34afe4b6ce0cc7eebb4a10f79b00bfda63951965
matbmoserufv <matbmoser@gmail.com>
2021-11-30 18:42:08 +0100

    Realizado Desarollo solución MPI

--- paralellGameMPI.py
+++ paralellGameMPI.py
@@ -1,15 +1,13 @@
-
-# to measure exec time
-from numba import njit, prange, set_num_threads, jit,int32, float32, boolean   # import the types
-from numba.experimental import jitclass
-from numba.np.ufunc import parallel
-import pygame
+from mpi4py import MPI
+import pyperclip
+import mpi4py
 import numpy as np
-from pygame import draw
 import time
-from numba import set_num_threads
 from timeit import default_timer as timer
 
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
 
 ## Global Shared Atributes
 background = (25, 25, 25) # Color de fondo
@@ -44,11 +42,11 @@ def open_display(w=400,h=400,nx=60,ny=60):
     #Definimos los parámetros para el display
     width = w
     height = h
-    screen = pygame.display.set_mode((height, width))
-    screen.fill(background)
+    #screen = pygame.display.set_mode((height, width))
+    #screen.fill(background)
     
     # Personalizamos la ventana 
-    pygame.display.set_caption("Horton's Game of Life: Mathias Moser y Rafael Camarero")
+    #pygame.display.set_caption("Horton's Game of Life: Mathias Moser y Rafael Camarero")
     
     #Numero de Celdas en cada eje
     nxC, nyC = nx, ny
@@ -66,6 +64,7 @@ def generate_random_board():
     global nxC, nyC
     return np.random.randint(2, size=(nxC, nyC))
 
+
 def calculate_neighbours(x, y):
     global gameState,nxC, nyC
     # Calcula el número de elementos alrededor sumando las posiciones
@@ -78,6 +77,7 @@ def calculate_neighbours(x, y):
             gameState[(x)     % nxC, (y + 1)  % nyC] + \
             gameState[(x + 1) % nxC, (y + 1)  % nyC]
 
+
 def getPolygon(x,y):
     global dimCH, dimCW
     # Calculamos el polígono que forma la celda.
@@ -85,7 +85,7 @@ def getPolygon(x,y):
 
 
 def startGame(w, h, nx, ny, epoch=0):
-    pygame.init()
+    #pygame.init()
     global gameState, epochs, counter
     # Pasamos los parámetros al tablero para abrir
     epochs = epoch
@@ -94,8 +94,9 @@ def startGame(w, h, nx, ny, epoch=0):
     
     while(True):
         
+        
         # Limpiamos la pantalla
-        clear_display()
+        #clear_display()
         
         checkStatus()
         
@@ -104,9 +105,8 @@ def startGame(w, h, nx, ny, epoch=0):
             break
         
         updateGameState()
-
         # Mostramos el resultado
-        pygame.display.flip()  
+        #pygame.display.flip()  
          
 def checkStatus():
     global finish, pauseExect, counter, epochs
@@ -116,64 +116,60 @@ def checkStatus():
         
     counter+=1
     # Registramos eventos de teclado y ratón.
-    
-    ev = pygame.event.get()
-
-    # PARALELIZAR -----------------------------
-    for event in ev:
-        
-        #Si damos a cerrar se cierra el tablero
-        if event.type == pygame.QUIT:
-            print("GameOfLife Closed!")
-            finish = True
-            return
-        
-        # Detectamos si se presiona una tecla.
-        if event.type == pygame.KEYDOWN:
-            
-            #En el caso de que la tecla sea escape o ESC pausamos el juego
-            if event.key == pygame.K_ESCAPE or event.key == pygame.K_SPACE:
-                pauseExect = not pauseExect
-
-            #En el caso que ta tecla sea CTRL + Z cerramos el juego
-            elif event.key == pygame.K_z:
-                mods = pygame.key.get_mods()
-                if mods & pygame.KMOD_CTRL:
-                    print("GameOfLife Closed!")
-                    finish = True
-        
 
 def updateGameState():
-    global gameState, nxC, tmpGame
+    global gameState, nxC, tmpGame,comm
     #Creamos una varible temporal que almacene el estado temporal de juego actual
     tmpGame = np.copy(gameState)
-
     # Recorremos las celdas en verticalmente
+    my_rank = comm.Get_rank()
+    world_size = comm.Get_size()
+
+    ## Actualizar GameState Global
+    if my_rank == 0:
+        gameState = comm.bcast(gameState, root=0)
+        
     #### Paralelizar------------------
     for y in range(0, nxC): 
-        # Recorremos las celdas en horizontalemente
+        # Recorremos las celdas en horizontal
         #### Paralizar------------------
-        tmpGame = vertical(y, tmpGame)
+        tmpGame = vertical(y, tmpGame)     
         #### ------------------        
-    #### ------------------  
+    #### ------------------ 
+    ## Juntamos el tablero en uno y guardamos cuando sea root 0 
     
-    gameState = tmpGame
+    # send results to rank 0
+    try:
+        if my_rank != 0:
+            resultsLen = [*range(my_rank,nxC, world_size)]
+            comm.Send(tmpGame[resultsLen], dest=0, tag=11)  # send results to process 0
+        else:
+            resultsLen= [*range(my_rank,nxC, world_size)]
+            final_result = np.zeros((nxC,nxC), dtype=int)  # create empty array to receive results
+            final_result[resultsLen] = tmpGame[resultsLen]
+            for i in range(1, world_size):  # determine the size of the array to be received from each process
+                tmpValues = [*range (i,nxC, world_size)]
+                comm.Recv(final_result[tmpValues], source=i, tag=11)  # receive results from the process
                 
+            tmpGame = final_result  
+            gameState = tmpGame
+            
+    except Exception as e:
+        print("[WARNING] Fail " % e)
 
-
-
+                
 def vertical(y, tmpGame):
-    global nyC, pauseExect,gameState, screen
-    for x in range (0, nyC):
-                
+    global nxC, nyC, pauseExect,gameState, screen, comm
+    # Dividimos en rangos
+    my_rank = comm.Get_rank()
+    world_size = comm.Get_size()
+    for x in range (my_rank, nyC, world_size):
             #Si el juego no esta en pausa
-            if not pauseExect:    
-                                
+            if not pauseExect:                    
                 ### APLICAMOS LAS REGLAS A UNA CELDA
                 
                 # Calculamos el número de vecinos cercanos.
                 num_neighbours = calculate_neighbours(x,y)
-                
                 
                 # Regla #1 : Una celda muerta con exactamente 3 vecinas vivas, "revive".
                 if gameState[x,y] == 0 and num_neighbours == 3:
@@ -183,26 +179,18 @@ def vertical(y, tmpGame):
                     tmpGame[x,y] = 0 
 
             # Calculamos el polígono que forma la celda.
-            poly = getPolygon(x,y)
-            
-            paint(poly,tmpGame[x,y])
-                
+            #poly = getPolygon(x,y)
+            # Envio el valor actualizado de TMPGAME A TODOS
+            #paint(poly,tmpGame[x,y])
+                 
     return tmpGame    
-
-def paint(poly, state):
-    global screen
-        # Si la celda está "muerta" pintamos un recuadro con borde gris
-    if state == 0:
-        pygame.draw.polygon(screen, (40, 40, 40), poly, 1)
-        return
-    
-    # Si la celda está "viva" pintamos un recuadro relleno de color
-    pygame.draw.polygon(screen, (255, 200, 100), poly, 0)  
-
 
 
 if __name__ == '__main__':
-
+    
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
     # Parámetros del Tablero:
     
     #Tamaño de la ventana del tablero 
@@ -210,17 +198,21 @@ if __name__ == '__main__':
     height = 800
     
     #Número de celdas en el eje X
-    nxC = 100
+    nxC = 50
     #Número de celdas en el eje Y
-    nyC = 100
+    nyC = 50
 
-    epochs = 250
+    epochs = 100
     
-
     #Empezamos el juego Paralelo
-    start = timer()
-    startGame(w=width, h=height, nx=nxC, ny=nyC, epoch=epochs)
-    end = timer()
-    print("Tiempo Secuencial: [{0}]".format(end-start))
-    
+    if rank == 0:
+        start = timer()
 
+    startGame(w=width, h=height, nx=nxC, ny=nyC, epoch=epochs)
+    
+    if rank == 0:
+        end = timer()
+        finalTime = end-start
+        print("Tiempo Paralelo: [{0}]".format(finalTime))
+        pyperclip.copy(finalTime)
+        
